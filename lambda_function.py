c01804809f84c4ecb870977c6d212fff66b79dcd
johnsipps <37401475+johnsipps@users.noreply.github.com>
2018-05-23 03:50:28 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,11 +5,11 @@ def lambda_handler(event,context):
 	
 	# Replace sender@example.com with your "From" address.
 	# This address must be verified with Amazon SES.
-	SENDER = "Sender Name <sippsin@gmail.com>"
+	SENDER = "Sender Name <sender@gmail.com>"
 	
 	# Replace recipient@example.com with a "To" address. If your account 
 	# is still in the sandbox, this address must be verified.
-	RECIPIENT = "sippsin@gmail.com"
+	RECIPIENT = "RECIPIENT@gmail.com"
 	
 	# Specify a configuration set. If you do not want to use a configuration
 	# set, comment the following variable, and the 
